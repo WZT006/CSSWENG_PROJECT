28222a1bf6c3368869de3582732ac31999eef3cf
WZT006 <60822592+WZT006@users.noreply.github.com>
2021-08-22 17:43:20 +0800

    Read/Filter columns

--- Project.py
+++ Project.py
@@ -4,21 +4,35 @@ import scipy as sp
 from collections import Counter
 
 def main():
-    read  = "Test"
-    file = read + ".xlsx"
-    dataset = pd.read_excel(open(file))
+    
+
+    ## to be changed to UI later on
+    tmp =  input("File name: ")
+    file = str(tmp) + ".xlsx"
+    sheet = input("Sheet Name: ")
+    
+    #read_file = pd.read_excel(file, skiprows=lambda x: x in [0, 1],usecols=lambda x: 'Unnamed' not in x)
+    #read_file.to_csv(r'Converted.csv', index = None, header=True)
+
+    dataset = pd.read_excel(file, skiprows=lambda x: x in [0, 1],usecols=lambda x: 'Unnamed' not in x,sheet_name = sheet)
     
     #reads companywhitelist
     with open("CompanyWhiteList.txt", "r", newline="\n") as cWhite:
         comWhite = cWhite.read().splitlines()     
     cWhite.close()
 
+    data = dataset[['Month','Group','AM','Client','Type','Solution Portfolio','Product',
+                    'Project','TOTAL Amount in Php', 'GP', "% GP", 'Date Received',
+                    'PO#','SO# 1st Round','HANA SO#', 'PS#', 'IO#', 'Ticket#', 'SOR#']]
+
 
 
     test = ["Globe","Smart","DATA"]
     #checks if there is companies outside company whitelist
     companyDirt= list((Counter(test)-Counter(comWhite)).elements())
-    print( companyDirt)
+    #print( companyDirt)
+
+    data.info()
 
 
 
