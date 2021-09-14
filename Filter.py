444a8652e83b61f2700a700232865ee36ca02bf4
WZT006 <60822592+WZT006@users.noreply.github.com>
2021-09-14 16:37:24 +0800

    directory changing

--- Filter.py
+++ Filter.py
@@ -4,28 +4,28 @@ import pandas as pd
 def filter(df):
     base_path = os.path.abspath(os.path.dirname(__file__))
     
-    file_path = os.path.join(base_path,"../Config/ClientWhiteList.txt")
+    file_path = os.path.join(base_path,"Config/ClientWhiteList.txt")
     with open(file_path, "r", newline="\n") as cols:
         filter = cols.read().splitlines()     
     cols.close()
     cInd = df[df.isin(list(set(df['Client'].values.tolist()) - set(filter))).any(axis=1)].index.tolist()
     cInd = [ 1+ i for i in cInd]
 
-    file_path = os.path.join(base_path,"../Config/AMWhiteList.txt")
+    file_path = os.path.join(base_path,"Config/AMWhiteList.txt")
     with open(file_path, "r", newline="\n") as cols:
         filter = cols.read().splitlines()     
     cols.close()
     AMInd = df[df.isin( list(set(df['AM'].values.tolist()) - set(filter)) ).any(axis=1)].index.tolist()
     AMInd = [ 1+ i for i in AMInd]    
 
-    file_path = os.path.join(base_path,"../Config/GroupWhiteList.txt")
+    file_path = os.path.join(base_path,"Config/GroupWhiteList.txt")
     with open(file_path, "r", newline="\n") as cols:
         filter = cols.read().splitlines()     
     cols.close()
     gInd = df[df.isin( list(set(df['Group'].values.tolist()) - set(filter)) ).any(axis=1)].index.tolist()
     gInd = [ 1+ i for i in gInd]    
 
-    file_path = os.path.join(base_path,"../Config/GroupWhiteList.txt")
+    file_path = os.path.join(base_path,"Config/GroupWhiteList.txt")
     with open(file_path, "r", newline="\n") as cols:
         filter = cols.read().splitlines()     
     cols.close()
